2aa495449ecaf394b5727382ead064cfa614de51
Khari Hopes <buildingkhari@yahoo.com>
2023-09-17 22:23:59 -0500

    fixing chart

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 'use strict';
 
-const imgVariable = {
-  products: [
-    new Product("bag", "img/bag.jpg"),
-    new Product("banana", "img/banana.jpg"),
-    new Product("bathroom", "img/bathroom.jpg"),
-    new Product("boots", "img/boots.jpg"),
-    new Product("breakfast", "img/breakfast.jpg"),
-    new Product("bubblegum", "img/bubblegum.jpg"),
+const products = [
+  new Product("bag", "img/bag.jpg"),
+  new Product("banana", "img/banana.jpg"),
+  new Product("bathroom", "img/bathroom.jpg"),
+  new Product("boots", "img/boots.jpg"),
+  new Product("breakfast", "img/breakfast.jpg"),
+  new Product("bubblegum", "img/bubblegum.jpg"),
   new Product("chair", "img/chair.jpg"),
   new Product("cthulhu", "img/cthulhu.jpg"),
   new Product("dog-duck", "img/dog-duck.jpg"),
@@ -21,155 +20,151 @@ const imgVariable = {
   new Product("unicorn", "img/unicorn.jpg"),
   new Product("water-can", "img/water-can.jpg"),
   new Product("wine-glass", "img/wine-glass.jpg"),
-],
+];
+
+let myChart = null;
+
+let imgVariable = {
+  products: [],
   imageContainer: document.getElementById("images"),
   resultsContainer: document.getElementById("results"),
-  previouslyShownProduct: [],
-  currentlyShownProduct: [],
+  currentlyShownProducts: [],
+  previouslyShownproducts: [],
 };
-
 
 imgVariable.imageContainer.addEventListener("click", handleClickProduct);
 
-function Product(productName, filePath, views, votes) {
+function Product(productName, filePath, views = 0, votes = 0) {
   this.productName = productName;
   this.filePath = filePath;
-  this.lastClicked = null;
-  this.votes = 0;
-  this.views = 0;
+  this.views = views;
+  this.votes = votes;
 }
- let myChart = null;
 
 Product.prototype.render = function () {
   const imgElm = document.createElement("img");
   imgElm.src = this.filePath;
   imgElm.alt = this.productName;
+
   imgVariable.imageContainer.appendChild(imgElm);
 };
 
 
+getLocalStorage();
+renderProducts();
+renderResults();
 
-function getRandomImg() {
-  imgVariable.imageContainer.innerHTML = '';
+function renderProducts() {
+  imgVariable.imageContainer.innerHTML = "";
 
-  imgVariable.previouslyShownProduct = imgVariable.currentlyShownProduct;
-  imgVariable.currentlyShownProduct = [];
+  imgVariable.previouslyShownproducts = [...imgVariable.currentlyShownProducts];
+  imgVariable.currentlyShownProducts = [];
 
-  while (imgVariable.currentlyShownProduct.length < 3) {
-    const randomInt = getRandomInt(0, imgVariable.products.length);
-    const randomProduct = imgVariable.products[randomInt];
+  while (imgVariable.currentlyShownProducts.length < 3) {
+    const randomInt = getRandomInt(0, products.length);
+    const randomProduct = products[randomInt];
     if (
-      !imgVariable.currentlyShownProduct.includes(randomProduct) &&
-      !imgVariable.previouslyShownProduct.includes(randomProduct)
+      !imgVariable.currentlyShownProducts.includes(randomProduct) &&
+      !imgVariable.previouslyShownproducts.includes(randomProduct)
     ) {
-      imgVariable.currentlyShownProduct.push(randomProduct);
+      imgVariable.currentlyShownProducts.push(randomProduct);
     }
   }
 
-  for (let i = 0; i < imgVariable.currentlyShownProduct.length; i++) {
-    imgVariable.currentlyShownProduct[i].views++;
-    imgVariable.currentlyShownProduct[i].render();
+  for (let i = 0; i < imgVariable.currentlyShownProducts.length; i++) {
+    imgVariable.currentlyShownProducts[i].views++;
+    imgVariable.currentlyShownProducts[i].render();
   }
 }
 
-
-getRandomImg();
-
 function handleClickProduct(event) {
   event.preventDefault();
+
   const target = event.target.alt;
 
   for (let i = 0; i < imgVariable.products.length; i++) {
     if (target === imgVariable.products[i].productName) {
       imgVariable.products[i].votes++;
-      imgVariable.products[i].views++;
     }
   }
-  if (imgVariable.currentlyShownProduct[0].views >= 6) {
-    renderChart();
-    renderResults();
-  }
-  getRandomImg();
+
+  setLocalStorage();
+  renderProducts();
+  renderResults();
 
 }
 
-getLocalStorage();
+function renderResults() {
+  imgVariable.resultsContainer.innerHTML = ""; 
+  renderChart();
+  const resultsElm = document.createElement("ul");
 
+  for (let i = 0; i < imgVariable.products.length; i++) {
+    const product = imgVariable.products[i];
 
-  function renderResults() {
-    imgVariable.resultsContainer.innerHTML = "";
-    const resultsElm = document.createElement("ul");
-
-    for (let i = 0; i < imgVariable.products.length; i++) {
-      const product = imgVariable.products[i];
-
-      const resultsItemElm = document.createElement("li");
-      resultsItemElm.textContent = `${product.productName} had ${product.votes} votes and was seen ${product.views} times.`;
-      resultsElm.appendChild(resultsItemElm);
-    }
-    
-    imgVariable.resultsContainer.appendChild(resultsElm);
+    const resultItemElm = document.createElement("li");
+    resultItemElm.textContent = `${product.productName} was seen ${product.views} times and was clicked ${product.votes} times.`;
+    resultsElm.appendChild(resultItemElm);
   }
 
-  function renderChart() {
-    const ctx = document.getElementById("myChart");
-  
-    const labels = [];
-    const votes = [];
-    const views = [];
-  
-    for (let i = 0; i < imgVariable.products.length; i++) {
-      const product = imgVariable.products[i];
-  
-      labels.push(product.productName);
-      votes.push(product.votes);
-      views.push(product.views);
-      
-      console.log("Labels:", labels);
-      console.log("Votes:", votes);
-      console.log("Views:", views);
-    }
-  
-    if ( myChart) {
-      myChart.clear();
-      myChart.destroy();
-    }
-  
-    imgVariable.myChart = new Chart(ctx, {
-      type: "bar",
-      data: {
-        labels: labels,
-        datasets: [
-          {
-            label: "# of Votes",
-            data: votes,
-            borderWidth: 1,
-          },
-          {
-            label: "# of Views",
-            data: views,
-            borderWidth: 1,
-          },
-        ],
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true,
-          },
+  imgVariable.resultsContainer.appendChild(resultsElm);
+}
+
+function renderChart() {
+  const ctx = document.getElementById("myChart");
+
+  const labels = [];
+  const votes = [];
+  const views = [];
+
+  for (let i = 0; i < imgVariable.products.length; i++) {
+    const product = imgVariable.products[i];
+
+    labels.push(product.productName); 
+    votes.push(product.votes);
+    views.push(product.views);
+  }
+
+  if (myChart) {
+    myChart.remove();
+    myChart.destroy(); 
+  }
+
+  myChart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: "# of Votes",
+          data: votes,
+          borderWidth: 1,
+        },
+        {
+          label: "# of Views",
+          data: views,
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
         },
       },
-    });
-  }
+    },
+  });
+}
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min) + min);
+  return Math.floor(Math.random() * (max - min + 1) + min); 
 }
 
 function setLocalStorage() {
-  localStorage.setItem("imgVariable", JSON.stringify(imgVariable));
+  localStorage.setItem("imgVariable", JSON.stringify(imgVariable)); 
 }
 
 function getLocalStorage() {
@@ -179,17 +174,17 @@ function getLocalStorage() {
     for (let i = 0; i < storedData.products.length; i++) {
       const product = storedData.products[i];
       const newProduct = new Product(
-        product.productName, 
-        product.filePath, 
-        product.views, 
+        product.productName,
+        product.filePath,
+        product.views,
         product.votes
       );
 
       imgVariable.products.push(newProduct);
     }
   } else {
-    for(let i = 0; i < imgVariable.products.length; i++){
-      imgVariable.products.push(imgVariable.products[i]);
+    for (let i = 0; i < products.length; i++) {
+      imgVariable.products.push(products[i]);
     }
   }
 }
